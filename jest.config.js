260034d3135318a8bdfe654a0264b4c95345a525
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2023-05-03 12:56:37 -0300

    fix: setup test

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
   transform: {
     "^.+\\.(t|j)sx?$": "@swc/jest",
   },
-  setupFiles: ["./src/@seedwork/__test__/setupFile.js"],
   testMatch: ["**/*.test.ts", "**/*.spec.ts"],
 };
